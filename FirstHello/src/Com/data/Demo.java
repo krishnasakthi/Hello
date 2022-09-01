e46ef9ee0d4cfda02744bb9e7a0498f4ce6677b2
krishna <krishna@DESKTOP-T51J9PB>
2022-09-01 22:41:22 +0530

    First operation

---
+++ FirstHello/src/Com/data/Demo.java
@@ -0,0 +1,29 @@
+package Com.data;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10;
+		
+		int b=20;
+		
+		int c=a+b;
+		
+		int d=a-b;
+		
+		int e=a*b;
+		
+		int f=a/b;
+		
+		System.out.println("Result of + :  "+c);
+		System.out.println("Result of - :  "+d);
+		System.out.println("Result of * :  "+e);
+		System.out.println("Result of / :  "+f);
+		
+		
+
+	}
+
+}
