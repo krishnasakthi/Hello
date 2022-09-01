0ffafff10eb930d1eb3744f8378dc734b2fe9908
krishna <krishna@DESKTOP-T51J9PB>
2022-09-01 22:56:21 +0530

    First Unary operator

---
+++ FirstHello/src/Com/data/Demo1.java
@@ -0,0 +1,19 @@
+package Com.data;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=100;
+		
+		System.out.println(a++);
+		System.out.println(a--);
+		System.out.println(++a);
+		System.out.println(--a);
+		
+		
+
+	}
+
+}
